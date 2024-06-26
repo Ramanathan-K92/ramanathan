8e667e20e23b1d0b11e29c2ff1334efc213ead34
Ramanathan-K92 <ramanathan1692@gmail.com>
2024-06-26 13:44:57 +0530

    Add files via upload

---
+++ monolithToMicroservice/src/test/java/com/ram/monolithToMicroservice/MonolithToMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.monolithToMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithToMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
